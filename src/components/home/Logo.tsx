6954e1ec7680297d4ae285e571661d51b6d74699
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-04-30 17:21:49 -0400

    Create Logo.tsx

---
+++ src/components/home/Logo.tsx
@@ -0,0 +1,13 @@
+export default function Logo({ width = 40, height = 40, className = '' }) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 500 500"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      {/* Paste your SVG <path> elements here */}
+    </svg>
+  );
+}
