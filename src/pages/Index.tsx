c5f957b297f4e497306afc5aac23e040c4131e5b
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-05-03 01:33:28 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,7 +7,7 @@ import SpaceSavingCategories from "@/components/home/SpaceSavingCategories";
 import FlashDeals from "@/components/home/FlashDeals";
 import SuperDeals from "@/components/home/SuperDealsSection";
 import ProductGrid from "@/components/home/ProductGrid";
-import ProductCarousel from '@/components/product/ProductCarousel';
+import ProductCarousel from '@/components/home/ProductCarrousel';
 import TopBrands from "@/components/home/TopBrands";
 import ProductRecommendations from "@/components/home/ProductRecommendations";
 import { useIsMobile } from "@/hooks/use-mobile";
