20ae0e1fb3a5aaf93a7dfb00624373c569404421
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-06-16 22:39:43 -0700

    Update TransferHeader.tsx

--- src/components/transfer/TransferHeader.tsx
+++ src/components/transfer/TransferHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Globe, Search, ChevronRight, Bell, MessageCircle } from 'lucide-react';
 
 const TransferHeader: React.FC = () => {
