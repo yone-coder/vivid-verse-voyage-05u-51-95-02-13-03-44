c7a27e4b4af4fb64e1c533b53aac6c4fb510b745
yone-coder <147109802+yone-coder@users.noreply.github.com>
2025-06-26 01:54:10 -0400

    Update PasswordAuthScreen.tsx

--- src/components/auth/PasswordAuthScreen.tsx
+++ src/components/auth/PasswordAuthScreen.tsx
@@ -135,7 +135,7 @@ const PasswordAuthScreen: React.FC<PasswordAuthScreenProps> = ({ email, onBack, 
           <div className="relative">
             <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
 
-            <input
+           <input
               id="password"
               type={showPassword ? 'text' : 'password'}
               value={password}
